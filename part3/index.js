aafeaca2ebcecb664f1f0c5b27fa43399f5fd6c6
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-23 12:40:06 -0400

    Part 3: Exercises 3.5-3.6

--- part3/index.js
+++ part3/index.js
@@ -70,7 +70,12 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
   const body = req.body
   if (!body.name || !body.number) {
-    return res.status(404).json({ error: 'Missing name or number' })
+    return res.status(400).json({ error: 'Missing name or number' })
+  }
+
+  const validatePerson = persons.find((p) => p.name === body.name)
+  if (validatePerson) {
+    return res.status(400).json({ error: 'Name must be unique' })
   }
 
   const newPerson = {
