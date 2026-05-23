0b66c995cf83f64ac433dedebae7491905b61bbf
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-23 14:12:13 -0400

    Part 3: Exercise 3.8

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -29,6 +30,7 @@ const generateID = () => {
 }
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/api/persons', (req, res) => {
   res.status(200).json(persons)
