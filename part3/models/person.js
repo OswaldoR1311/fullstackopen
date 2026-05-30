79454a5292f871c071cc4484fec4ab45cb2b6414
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-29 20:27:26 -0400

    Part 3: Exercise 3.19

--- part3/models/person.js
+++ part3/models/person.js
@@ -7,11 +7,14 @@ const url = process.env.MONGODB_URI
 console.log('connecting to url', url)
 mongoose
   .connect(url, { family: 4 })
-  .then((result) => console.log('connected to MongoDB'))
-  .catch((error) => console.log('error connecting to MongoDB', error.message))
+  .then(result => console.log('connected to MongoDB'))
+  .catch(error => console.log('error connecting to MongoDB', error.message))
 
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    minLength: 3,
+    required: true,
+  },
   number: String,
 })
 
